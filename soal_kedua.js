9d1aee53cd129dcf681e86630ef8ad09274e13f7
Natannegara <natannegara@gmail.com>
2021-04-06 10:51:08 +0700

    completing task (2)

--- soal_kedua.js
+++ soal_kedua.js
@@ -12,19 +12,44 @@ const split_comma = split_sentence.map(v => {
     return v.split(',')
 })
 
+// VERSION 1
 // find censor word in each word of paragraph
-const final = split_comma.map(sentence => {
-    var join_sentence = sentence.map(comma => {
-        var per_comma = comma.split(' ')
-        let join_comma = per_comma.map((word, i) => {
-            if (censor_word.includes(word)) {
-                return per_comma[i] = '*'.repeat(word.length)
-            } else {
-                return per_comma[i]
-            }
+// const final = split_comma.map(sentence => {
+//     var join_sentence = sentence.map(comma => {
+//         var per_comma = comma.split(' ')
+//         let join_comma = per_comma.map((word, i) => {
+
+//             if (censor_word.includes(word)) {
+//                 return per_comma[i] = '*'.repeat(word.length)
+//             } else {
+//                 return per_comma[i]
+//             }
+//         })
+//         return join_comma.join(' ');
+//     })
+//     return join_sentence.join(',')
+// })
+// console.log(final.join(' '));
+
+// VERSION 2
+const result = split_comma.map(sentence => {
+    var line_three = sentence.map(comma => {
+        var line_two = []
+        comma.split(' ').map(line => {
+            var line_one = []
+            line.split('.').map(x => {
+                if (x != '') {
+                    if (censor_word.includes(x)) {
+                        line_one.push('*'.repeat(x.length))
+                    } else {
+                        line_one.push(x)
+                    }
+                }
+            })
+            line_two.push(line_one)
         })
-        return join_comma.join(' ');
+        return line_two.join(' ');
     })
-    return join_sentence.join(',')
+    return line_three.join(',')
 })
-console.log(final.join(' '));
+console.log(result.join('. \n') + '.');
